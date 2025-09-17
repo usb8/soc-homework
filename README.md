3be49ed5e2fa8496a99eae740742b6a257217595
Harry Nguyen <hieu.fs8@gmail.com>
2025-09-17 20:06:28 +0300

    set up env

---
+++ README.md
@@ -0,0 +1,10 @@
+```shell
+python -m pip install virtualenv  # python3 -m pip install virtualenv
+python -m virtualenv .venv
+
+.venv\Scripts\activate  # source .venv/bin/activate (for Linux CLI)
+  deactivate
+pip install -r requirements.txt  # pip install <lib>; pip freeze > requirements.txt
+
+python <file>.py
+```
---
+++ ex1.py
@@ -0,0 +1,13 @@
+# SQLite library for python for reading, updating database
+# See documentation at https://docs.python.org/3/library/sqlite3.html
+import sqlite3
+
+# Pandas library for data analysis and manipulation. Not specific for SQL, but provides support for importing data from SQLite database file.
+# Seed documentation at https://pypi.org/project/pandas/
+import pandas as pd
+
+# # Optional for clearing the terminal at each run
+# import os
+# os.system('clear')
+
+print("Hi")
